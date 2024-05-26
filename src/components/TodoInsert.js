99a8f9067d95072d7865ff44432875551d905923
torong <hi_ryuji@naver.com>
2024-05-26 10:17:41 +0900

    feat: 항목 추가 기능 구현 - TodoInsert value 상태 관리 - todos 배열에 새 객체 추가 - TodoInsert에서 onSubmit 이벤트 설정

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,10 +1,32 @@
+import { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue(''); // value 값 초기화
+
+      // submit 이벤트는 브라우저에서 새로고침을 발생시키므로 이 함수로 방지함
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
+
   return (
-    <form className="TodoInsert">
-      <input placeholder="할 일을 입력하세요" />
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
       <button type="submit">
         <MdAdd />
       </button>
